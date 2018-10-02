684bc574f1329ac900901a0f7a310eeb62b87658
Joseph Lee <cgjosephlee@gmail.com>
2018-10-02 12:09:06 +0800

    initial commit

---
+++ increment_decrement.py
@@ -0,0 +1,36 @@
+import sublime
+import sublime_plugin
+
+class NumberCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        selection = self.view.sel()
+        for region in selection:
+            try:
+                # Try to operate on around the cursor if nothing is selected
+                if region.empty():
+                    begin = region.begin()
+                    while begin >= 0:
+                        if not self.view.substr(begin - 1).isdigit():
+                            break
+                        begin = begin - 1
+                    end = region.end()
+                    while end < self.view.size():
+                        if not self.view.substr(end).isdigit():
+                            break
+                        end = end + 1
+                    region = sublime.Region(begin, end)
+                value = int(self.view.substr(region))
+                self.view.replace(edit, region, str(self.op(value)))
+            except ValueError:
+                pass
+
+    def is_enabled(self):
+        return len(self.view.sel()) > 0
+
+class IncrementCommand(NumberCommand):
+    def op(self, value):
+        return value + 1
+
+class DecrementCommand(NumberCommand):
+    def op(self, value):
+        return value - 1
---
+++ readme.md
@@ -0,0 +1,26 @@
+# Increment Decrement
+Sublime Text plugin to increase/decrease selected numbers
+
+## Features
+Works like `C-a` and `C-x` in vim, increase or decrease the selected numbers or number around the cursor by `+1` or `-1`.
+
+## Installation
+Clone the repo to your package folder `Preferences -> Browse Packages`.
+
+## Usage
+Tip: Use multi-cursors to select multiple numbers.
+
+### Key-bindings (OSX)
+- <kbd>cmd</kbd> + <kbd>opt</kbd> + <kbd>a</kbd> : increase the numbers
+- <kbd>cmd</kbd> + <kbd>opt</kbd> + <kbd>x</kbd> : decrease the numbers
+
+### Command palette
+Hit <kbd>cmd</kbd> + <kbd>shift</kbd> + <kbd>p</kbd> and search `increment` or `decrement`.
+
+## Contributors
+- [dtao (original author)](https://gist.github.com/dtao/2788978)
+- [halilim (implement searching while nothing selected)](https://gist.github.com/dtao/2788978#gistcomment-790682)
+- cgjosephlee
+
+## License
+MIT
