cd606fb64fe8b1a240d44df677283745417fa5d2
Joseph Lee <cgjosephlee@gmail.com>
2018-10-19 00:19:56 +0800

    new features

--- increment_decrement.py
+++ increment_decrement.py
@@ -8,21 +8,61 @@ class NumberCommand(sublime_plugin.TextCommand):
             try:
                 # Try to operate on around the cursor if nothing is selected
                 if region.empty():
-                    begin = region.begin()
-                    while begin >= 0:
-                        if not self.view.substr(begin - 1).isdigit():
-                            if self.view.substr(begin - 1) == '-':
-                                begin -= 1
-                            break
-                        begin -= 1
-                    end = region.end()
-                    while end < self.view.size():
-                        if not self.view.substr(end).isdigit():
-                            break
-                        end += 1
-                    region = sublime.Region(begin, end)
-                value = int(self.view.substr(region))
-                self.view.replace(edit, region, str(self.op(value)))
+                    begin = end = region.begin()
+                    guess_type = 'int'
+                    if self.view.substr(begin - 1).isdigit():
+                        while begin >= 0:
+                            if not self.view.substr(begin - 1).isdigit():
+                                # integer
+                                if self.view.substr(begin - 1) == '-':
+                                    # guess_type = 'int'
+                                    begin -= 1
+                                    break
+                                # decimal
+                                elif self.view.substr(begin - 1) == '.':
+                                    if guess_type == 'dec':  # meet second time, maybe version or IP?
+                                        break
+                                    guess_type = 'dec'
+                                    begin -= 1
+                                    continue
+                                # hexadecimal, not implemented
+                                # elif self.view.substr(begin - 1) == 'x':
+                                #     guess_type = 'hex'
+                                #     pass
+                                break
+                            begin -= 1
+                        while end < self.view.size():
+                            if not self.view.substr(end).isdigit():
+                                if self.view.substr(end) == '.':
+                                    if guess_type == 'int':
+                                        end += 1
+                                        guess_type = 'dec'
+                                        continue
+                                    elif guess_type == 'dec':
+                                        break
+                                break
+                            end += 1
+                        region = sublime.Region(begin, end)
+                    elif self.view.substr(begin - 1).isalpha():
+                        region = self.view.word(region)
+                        if self.view.substr(region) in ['TRUE', 'True', 'true', 'FALSE', 'False', 'false']:
+                            guess_type = 'bool'
+                        else:
+                            raise ValueError
+                    value = self.view.substr(region)
+                else:
+                    import re
+                    value = self.view.substr(region)
+                    if value in ['TRUE', 'True', 'true', 'FALSE', 'False', 'false']:
+                        guess_type = 'bool'
+                    elif re.match(r'[-]?\d+$', value):
+                        guess_type = 'int'
+                    elif re.match(r'\d+\.\d+$', value):
+                        guess_type = 'dec'
+                    else:
+                        raise ValueError
+                self.view.replace(edit, region, str(self.op(value, guess_type)))
+                # print(value, str(self.op(value, guess_type)), guess_type)
             except ValueError:
                 pass
 
@@ -30,9 +70,29 @@ class NumberCommand(sublime_plugin.TextCommand):
         return len(self.view.sel()) > 0
 
 class IncrementCommand(NumberCommand):
-    def op(self, value):
-        return value + 1
+    def op(self, value, guess_type):
+        if guess_type == 'int':
+            value = int(value)
+            value += 1
+        elif guess_type == 'dec':
+            dec_part = len(value.split('.')[-1])
+            value = '{:.{dec}f}'.format(float(value) + (10 ** -dec_part), dec=dec_part)
+        elif guess_type == 'bool':
+            convert_bool = {'TRUE': 'FALSE', 'True': 'False', 'true': 'false',
+                            'FALSE': 'TRUE', 'False': 'True', 'false': 'true'}
+            value = convert_bool[value]
+        return value
 
 class DecrementCommand(NumberCommand):
-    def op(self, value):
-        return value - 1
+    def op(self, value, guess_type):
+        if guess_type == 'int':
+            value = int(value)
+            value -= 1
+        elif guess_type == 'dec':
+            dec_part = len(value.split('.')[-1])
+            value = '{:.{dec}f}'.format(float(value) - (10 ** -dec_part), dec=dec_part)
+        elif guess_type == 'bool':
+            convert_bool = {'TRUE': 'FALSE', 'True': 'False', 'true': 'false',
+                            'FALSE': 'TRUE', 'False': 'True', 'false': 'true'}
+            value = convert_bool[value]
+        return value
--- readme.md
+++ readme.md
@@ -1,18 +1,37 @@
 # Increment Decrement
-Sublime Text plugin to increase/decrease selected numbers
+Sublime Text plugin to increase/decrease selected values (int, float, bool).
 
 ## Features
-Works like `C-a` and `C-x` in vim, increase or decrease the selected numbers or number around the cursor by `+1` or `-1`.
+Inspired by `C-a` and `C-x` in vim and [nextval](https://www.vim.org/scripts/script.php?script_id=4536).
+```
+value -> decrement/increment
+5     -> 4/6
+-1    -> -2/0
+4.99  -> 4.98/5.00
+TURE  -> FALSE/FALSE
+Ture  -> False/False
+false -> true/true
+```
 
 ## Installation
-Clone the repo to your package folder `Preferences -> Browse Packages`.
+Clone this repo to your package folder `Preferences -> Browse Packages`.
+
+*Currently not avalible in package control channel.*
 
 ## Usage
-Tip: Use multi-cursors to select multiple numbers.
+Place the cursor around the value or select the wanted value, then hit the keystroke!
+
+Tip: Use multi-cursors to select multiple values.
+- Select a region and hit <kbd>cmd</kbd> + <kbd>shift</kbd> + <kbd>l</kbd> to place cursors to every EOL ([official doc](https://www.sublimetext.com/docs/3/multiple_selection_with_the_keyboard.html)).
+- Hold <kbd>cmd</kbd> ( <kbd>ctrl</kbd> on windows) and click to place multiple cursors.
 
 ### Key-bindings (OSX)
-- <kbd>cmd</kbd> + <kbd>opt</kbd> + <kbd>a</kbd> : increase the numbers
-- <kbd>cmd</kbd> + <kbd>opt</kbd> + <kbd>x</kbd> : decrease the numbers
+- <kbd>cmd</kbd> + <kbd>opt</kbd> + <kbd>a</kbd> : increase the values
+- <kbd>cmd</kbd> + <kbd>opt</kbd> + <kbd>x</kbd> : decrease the values
+
+### Key-bindings (Windows)
+- <kbd>ctrl</kbd> + <kbd>alt</kbd> + <kbd>a</kbd> : increase the values
+- <kbd>ctrl</kbd> + <kbd>alt</kbd> + <kbd>x</kbd> : decrease the values
 
 ### Command palette
 Hit <kbd>cmd</kbd> + <kbd>shift</kbd> + <kbd>p</kbd> and search `increment` or `decrement`.
@@ -20,7 +39,6 @@ Hit <kbd>cmd</kbd> + <kbd>shift</kbd> + <kbd>p</kbd> and search `increment` or `
 ## Contributors
 - [dtao (original author)](https://gist.github.com/dtao/2788978)
 - [halilim (implement searching while nothing selected)](https://gist.github.com/dtao/2788978#gistcomment-1246653)
-- cgjosephlee
 
 ## License
 MIT
