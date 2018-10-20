f6c1d03ed69c29d60c8a5462fc9b89254928653c
Joseph Lee <cgjosephlee@gmail.com>
2018-10-21 00:34:50 +0800

    add new feature (Roman numerals)

--- increment_decrement.py
+++ increment_decrement.py
@@ -1,5 +1,46 @@
 import sublime
 import sublime_plugin
+import re
+
+def roman(RomanNum, plus):
+    decimalDens = [100, 90, 50, 40, 10, 9, 5, 4, 1]
+    romanDens = ["C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
+    # convert to decimal
+    if re.match(r'[IVXLC]+', RomanNum):
+        decimalSum = 0
+        # romanLen = len(RomanNum)
+        currentLen = 0
+        while currentLen < len(RomanNum):
+            for R in romanDens:
+                if re.match(R, RomanNum[currentLen:]):
+                    currentLen += len(R)
+                    decimalSum += decimalDens[romanDens.index(R)]
+        # currently not support > 100
+        if decimalSum > 100:
+            raise ValueError
+    else:
+        raise ValueError
+    # plus or minus
+    if plus:
+        if decimalSum < 100:
+            decimalSum += 1
+        else:
+            raise ValueError
+    else:
+        if decimalSum > 1:
+            decimalSum -= 1
+        else:
+            raise ValueError
+    # convert to roman
+    NewRomanNum = ''
+    while decimalSum > 0:
+        for N in decimalDens:
+            if decimalSum >= N:
+                decimalSum -= N
+                NewRomanNum += romanDens[decimalDens.index(N)]
+                break
+    return NewRomanNum
+
 
 class NumberCommand(sublime_plugin.TextCommand):
     def run(self, edit):
@@ -45,16 +86,21 @@ class NumberCommand(sublime_plugin.TextCommand):
                         region = sublime.Region(begin, end)
                     elif self.view.substr(begin - 1).isalpha():
                         region = self.view.word(region)
+                        # boolean
                         if self.view.substr(region) in ['TRUE', 'True', 'true', 'FALSE', 'False', 'false']:
                             guess_type = 'bool'
+                        # roman numeral
+                        elif re.match(r'[IVXLC]+', self.view.substr(region)):
+                            guess_type = 'roman'
                         else:
                             raise ValueError
                     value = self.view.substr(region)
                 else:
-                    import re
                     value = self.view.substr(region)
                     if value in ['TRUE', 'True', 'true', 'FALSE', 'False', 'false']:
                         guess_type = 'bool'
+                    elif re.match(r'[IVXLC]+', self.view.substr(region)):
+                        guess_type = 'roman'
                     elif re.match(r'[-]?\d+$', value):
                         guess_type = 'int'
                     elif re.match(r'[-]?\d+\.\d+$', value):
@@ -69,6 +115,7 @@ class NumberCommand(sublime_plugin.TextCommand):
     def is_enabled(self):
         return len(self.view.sel()) > 0
 
+
 class IncrementCommand(NumberCommand):
     def op(self, value, guess_type):
         if guess_type == 'int':
@@ -81,7 +128,10 @@ class IncrementCommand(NumberCommand):
             convert_bool = {'TRUE': 'FALSE', 'True': 'False', 'true': 'false',
                             'FALSE': 'TRUE', 'False': 'True', 'false': 'true'}
             value = convert_bool[value]
+        elif guess_type == 'roman':
+            value = roman(value, True)
         return value
+
 
 class DecrementCommand(NumberCommand):
     def op(self, value, guess_type):
@@ -95,4 +145,6 @@ class DecrementCommand(NumberCommand):
             convert_bool = {'TRUE': 'FALSE', 'True': 'False', 'true': 'false',
                             'FALSE': 'TRUE', 'False': 'True', 'false': 'true'}
             value = convert_bool[value]
+        elif guess_type == 'roman':
+            value = roman(value, False)
         return value
--- readme.md
+++ readme.md
@@ -1,22 +1,27 @@
 # Increment Decrement
-Sublime Text plugin to increase/decrease selected values (int, float, bool).
+Sublime Text plugin to increase/decrease selected values (int, float, roman, bool).
 
 ## Features
 Inspired by `C-a` and `C-x` in vim and [nextval](https://www.vim.org/scripts/script.php?script_id=4536).
-```
-value -> decrement/increment
-5     -> 4/6
--1    -> -2/0
-4.99  -> 4.98/5.00
-TURE  -> FALSE/FALSE
-Ture  -> False/False
-false -> true/true
-```
+
+| value | decrement | increment|
+|-------|-----------|----------|
+| 5     | 4         | 6        |
+| -1    | -2        | 0        |
+| 4.99  | 4.98      | 5.00     |
+| -1.1  | -1.2      | -1.0     |
+| III   | II        | IV       |
+| IX    | VIII      | X        |
+| TRUE  | FALSE     | FALSE    |
+| True  | False     | False    |
+| false | true      | true     |
+
+Caveats: Roman numerals > 100 (rarely used) or < 1 (no applicable) are not supported.
 
 ## Installation
 Clone this repo to your package folder `Preferences -> Browse Packages`.
 
-*Currently not avalible in package control channel.*
+*Currently not avalible in package control channel. I'm working on it.*
 
 ## Usage
 Place the cursor around the value or select the wanted value, then hit the keystroke!
@@ -26,10 +31,10 @@ Tip: Use multi-cursors to select multiple values.
 - Hold <kbd>cmd</kbd> ( <kbd>ctrl</kbd> on windows) and click to place multiple cursors.
 
 ### Key-bindings (OSX)
-- <kbd>cmd</kbd> + <kbd>opt</kbd> + <kbd>a</kbd> : increase the values
-- <kbd>cmd</kbd> + <kbd>opt</kbd> + <kbd>x</kbd> : decrease the values
+- <kbd>cmd</kbd> + <kbd>ctrl</kbd> + <kbd>a</kbd> : increase the values
+- <kbd>cmd</kbd> + <kbd>ctrl</kbd> + <kbd>x</kbd> : decrease the values
 
-### Key-bindings (Windows)
+### Key-bindings (Windows, Linux)
 - <kbd>ctrl</kbd> + <kbd>alt</kbd> + <kbd>a</kbd> : increase the values
 - <kbd>ctrl</kbd> + <kbd>alt</kbd> + <kbd>x</kbd> : decrease the values
 
